fc3fe1a71e35073a6e59b9d7a31c0fdd740a91da
Xushnud Eshtemirov <xushnud122997@gmail.com>
2024-12-26 11:34:23 +0500

    Created first angular app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, HeaderComponent, HomeComponent],
+  template: `
+    <app-header />
+    <app-home />
+  `,
 })
 export class AppComponent {
   title = 'first-ng-app';
